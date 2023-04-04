7035d184364c46c1ecf8987aa1aa988a1b42e2d8
Carina Cunha <carina.cunha13@gmail.com>
2023-04-04 15:06:06 -0300

    feat: create a test_product

--- tests/product/test_product.py
+++ tests/product/test_product.py
@@ -1,5 +1,18 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
+
+mock = {
+    "id": 1,
+    "nome_do_produto": "Borracha",
+    "nome_da_empresa": "Papelaria Solar",
+    "data_de_fabricacao": "2021-07-04",
+    "data_de_validade": "2029-02-09",
+    "numero_de_serie": "FR48",
+    "instrucoes_de_armazenamento": "Ao abrigo de luz solar"
+}
 
 
 def test_cria_produto():
-    pass  # Seu teste deve ser escrito aqui
+    product = Product(**mock)
+
+    for key, value in mock.items():
+        assert getattr(product, key) == value
