188d6425bf549cd6f8e1e4a66e5567ae1d0d5d23
Carina Cunha <carina.cunha13@gmail.com>
2023-04-04 16:44:08 -0300

    feat: implmensts simple_report function

--- inventory_report/reports/simple_report.py
+++ inventory_report/reports/simple_report.py
@@ -1,2 +1,22 @@
+import statistics
+from datetime import datetime
+
+
 class SimpleReport:
-    pass
+    @classmethod
+    def generate(cls, products):
+        oldest_manuf_date = min(
+            [product["data_de_fabricacao"] for product in products])
+
+        closest_exp_date = min(
+            [product["data_de_validade"] for product in products
+                if datetime.strptime(product["data_de_validade"], "%Y-%m-%d")
+                >= datetime.today()])
+
+        companies = [product["nome_da_empresa"] for product in products]
+
+        return (
+            f"Data de fabricação mais antiga: {oldest_manuf_date}\n"
+            f"Data de validade mais próxima: {closest_exp_date}\n"
+            f"Empresa com mais produtos: {statistics.mode(companies)}"
+        )
